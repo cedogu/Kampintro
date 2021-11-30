12b31ccda5bef533f5f3a4e3ee39e2f57c28ff22
cedogu <86116050+cedogu@users.noreply.github.com>
2021-12-01 00:25:17 +0300

    OOP

---
+++ OOP1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //first way
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Table";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            //second way
+            Product product2 = new Product { Id = 2, CategoryId = 5, ProductName = "Pencil", UnitsInStock = 5, UnitPrice = 35 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+
+
+        }
+    }
+}
