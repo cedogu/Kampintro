d1940d1145cdcbaeb588da6977ec44b385c1e0cb
cedogu <86116050+cedogu@users.noreply.github.com>
2021-12-01 17:02:59 +0300

    ClassMethodDemo Homework

---
+++ ClassMethodDemoHW/ClassMethodDema/ClassMethodDema/CustomerManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDema
+{
+    class CustomerManager
+    {
+        public void Add(Customer c)
+        {
+            Console.WriteLine("Customer Information: " + c.CustomerName, c.CustomerSurname, c.CustomerOccupation);
+        }
+        public void Delete(Customer c)
+        {
+            Console.WriteLine("Customer Deleted " + c.CustomerName, c.CustomerSurname, c.CustomerOccupation);
+        }
+    }
+}
---
+++ ClassMethodDemoHW/ClassMethodDema/ClassMethodDema/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassMethodDema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.CustomerId = 1;
+            customer1.CustomerName = "Cem Doruk";
+            customer1.CustomerSurname = "Gürsoy";
+            customer1.CustomerOccupation = " Pilot";
+
+            Customer customer2 = new Customer();
+
+            customer2.CustomerId = 2;
+            customer2.CustomerName = "Nehir";
+            customer2.CustomerSurname = "Gül";
+            customer2.CustomerOccupation = " Lawyer";
+
+            Customer[] customers = new Customer[] { customer1, customer2 };
+
+            foreach (var mycustomer in customers)
+            {
+                Console.WriteLine(mycustomer.CustomerName + " " + mycustomer.CustomerSurname + "" + mycustomer.CustomerOccupation);
+            }
+            Console.WriteLine("----------------");
+
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Delete(customer1);
+
+            customerManager.Add(customer2);
+            customerManager.Delete(customer2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
