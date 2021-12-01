d1a770e237113e326c711c9eacb91b095399c982
cedogu <86116050+cedogu@users.noreply.github.com>
2021-12-01 15:43:22 +0300

    1 Aralık 2021 Generic Listesi

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items = new T[] { };
+
+        //constructor, aslında bir methodtur.class new denildikce otomatik calısır.
+        //Constructor class ile aynı isimlere sahiptir.
+        //Temp yapmamızın sebebi; New yaptıgımız zaman ref tipliye döndügü icin bir önceki silinir, bir önceki değerlerin silinmemesi icin de biz ona TempArray veririz
+        //böylelikle de değerler kaybolmadan length'i +1 arttırabiliriz.
+        //geçici olarak TempArray'da tuttugumuz değerleri de items'a eklemek için for içine bir döngü yaparız ve onları eşleştiririz, Aşağıda yaptıgımız gibi. items[i]=tempArray[i] 
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Doruk");
+
+            List<string> liste = new List<string>();
+            Console.WriteLine(liste.Count);
+
+
+            //triallllllllllllll
+            //List<string> liste = new List<string>();
+            //liste.Add("doruk");
+            //Console.WriteLine(liste.Count);
+
+            Console.WriteLine("Hello World!");
+
+
+
+            //MyList classında Add isminde method tanımlıyoruz, onun türünü T item yapıyoruz
+            //T = type'dan gelir ve herhangi bir type'i kabul edeceğini programa söylüyoruz.
+            //Program.cs'ye geldigimizde Mylist<string>, Mylist<boolean>, MyList<int> vb. herhangi bir tipte listemizi/generik olusturabiliriz.
+        }
+    }
+}
