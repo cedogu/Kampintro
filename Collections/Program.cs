4a5bfc29bc8caed5a30f31fbd5ac41a15e8d6c2b
cedogu <86116050+cedogu@users.noreply.github.com>
2021-12-01 14:41:36 +0300

    Collections

---
+++ Collections/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"Doruk","Cem","Mehmet","Gökhan","Serhat","Mehmet2"};
+
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[5]);
+
+            //Console.WriteLine("-----------------------------");
+
+            //isimler = new string[6];
+            //isimler[4] = "Süleyman";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+
+            List<string> isimler2 = new List<string> { "Doruk", "Cem", "Mehmet", "Gökhan",  };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Serhat");
+            Console.WriteLine(isimler2[4]);
+          
+
+        }
+    }
+}
