e90ee1396e94b7e9d17f95f6bdbfb77e903526e3
cedogu <86116050+cedogu@users.noreply.github.com>
2021-12-05 13:24:58 +0300

    After I added MernisService to my Project unfortunately it blocked my Save method and it couldn't work but NeroCustomerManager works properly. After I checked the solution I found that Mernis system reference had been changed, so shortly, my source reference (Mernis website) is pretty old! not my mistake :)) Have fun...

---
+++ InterfaceAbstractDemo/Concrete/StarbucksCustomerManager.cs
@@ -0,0 +1,35 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo.Concrete
+{
+    public class StarbucksCustomerManager:BaseCustomerManager
+    {
+
+        ICustomerCheckService _customercheckService;
+
+        public StarbucksCustomerManager (ICustomerCheckService customerCheckService)
+        {
+            _customercheckService = customerCheckService;
+        }
+        public override void Save(Customer customer)
+        {
+            if (_customercheckService.CheckIfRealPerson(customer))
+            {
+              base.Save(customer);
+            }
+            else
+            {
+                throw new Exception("Not a valid person");
+            }
+            
+        }
+
+
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Entities.Customer { DateofBirth=new System.DateTime(1995,7,20), FirstName="Cem Doruk", LastName="Gürsoy", NationalityId="1234567890"});
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
